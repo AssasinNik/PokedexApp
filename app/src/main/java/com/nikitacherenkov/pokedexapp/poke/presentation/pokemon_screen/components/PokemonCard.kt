91842c571fbdbe81eed5cfaf6117a847d471d022
AssasinNik <nikitacherenkov228@gmail.com>
2024-11-19 13:04:49 +0300

    Update Card

--- app/src/main/java/com/nikitacherenkov/pokedexapp/poke/presentation/pokemon_screen/components/PokemonCard.kt
+++ app/src/main/java/com/nikitacherenkov/pokedexapp/poke/presentation/pokemon_screen/components/PokemonCard.kt
@@ -2,21 +2,14 @@ package com.nikitacherenkov.pokedexapp.poke.presentation.pokemon_screen.componen
 
 import android.os.Build
 import androidx.compose.animation.AnimatedContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -30,20 +23,17 @@ import coil.request.ImageRequest
 import com.nikitacherenkov.pokedexapp.R
 import com.nikitacherenkov.pokedexapp.poke.domain.PokemonInfo
 import kotlinx.coroutines.Dispatchers
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Height
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.filled.FitnessCenter
-import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.nikitacherenkov.pokedexapp.poke.domain.Stat
-import com.nikitacherenkov.pokedexapp.poke.domain.Type
 
-@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun PokemonCard(
     pokemon: PokemonInfo?
@@ -57,96 +47,51 @@ fun PokemonCard(
         Color.White
     }
 
-    GradientBackground() {
-        Box(modifier = Modifier.fillMaxSize()) {
+    GradientBackground {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ) {
+            // Изображение покемона
             Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .verticalScroll(rememberScrollState(), enabled = true)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-
-                // Изображение и типы
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Top
-                ) {
-                    AnimatedContent(
-                        targetState = pokemon.imageUrl,
-                        modifier = Modifier.align(Alignment.CenterHorizontally),
-                        label = ""
-                    ) { icon ->
-                        PokemonImage(imageUrl = icon, primaryType = primaryType.toString())
-                    }
-                }
-
-                Spacer(modifier = Modifier.height(16.dp))
-                BoxWithConstraints(
-                    modifier = Modifier.fillMaxWidth(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    val maxWidth = this.maxWidth
-                    val threshold = 600.dp // Пороговая ширина для переключения раскладки
-
-                    if (maxWidth >= threshold) {
-                        // Раскладка в строку
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.Center,
-                            verticalAlignment = Alignment.Top
-                        ) {
-                            PokemonInfoCard(
-                                title = stringResource(id = R.string.overview),
-                                content = {
-                                    OverviewSection(pokemon = pokemon, contentColor = contentColor)
-                                },
-                                modifier = Modifier
-                                    .weight(1f)
-                                    .padding(8.dp)
-                                    .requiredWidth(280.dp) // Минимальная ширина
-                            )
-                            PokemonInfoCard(
-                                title = stringResource(id = R.string.stats),
-                                content = {
-                                    StatsSection(stats = pokemon.stats, contentColor = contentColor, onStatClick = {})
-                                },
-                                modifier = Modifier
-                                    .weight(1f)
-                                    .padding(8.dp)
-                                    .requiredWidth(280.dp) // Минимальная ширина
-                            )
-                        }
-                    } else {
-                        // Раскладка в столбец
-                        Column(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            PokemonInfoCard(
-                                title = stringResource(id = R.string.overview),
-                                content = {
-                                    OverviewSection(pokemon = pokemon, contentColor = contentColor)
-                                },
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(8.dp)
-                                    .requiredHeight(200.dp) // Минимальная высота
-                            )
-                            PokemonInfoCard(
-                                title = stringResource(id = R.string.stats),
-                                content = {
-                                    StatsSection(stats = pokemon.stats, contentColor = contentColor, onStatClick = {})
-                                },
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(8.dp)
-                                    .requiredHeight(200.dp) // Минимальная высота
-                            )
-                        }
-                    }
+                AnimatedContent(
+                    targetState = pokemon.imageUrl,
+                    label = ""
+                ) { icon ->
+                    PokemonImage(imageUrl = icon)
                 }
             }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Отодвигаем карточки вниз
+            Spacer(modifier = Modifier.weight(1f))
+
+            // Карточки информации
+            PokemonInfoCard(
+                title = stringResource(id = R.string.overview),
+                content = {
+                    OverviewSection(pokemon = pokemon, contentColor = contentColor)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+
+            PokemonInfoCard(
+                title = stringResource(id = R.string.stats),
+                content = {
+                    StatsSection(stats = pokemon.stats, contentColor = contentColor)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
         }
     }
 }
@@ -164,7 +109,7 @@ fun GradientBackground(
 }
 
 @Composable
-fun PokemonImage(imageUrl: String, primaryType: String) {
+fun PokemonImage(imageUrl: String) {
     SubcomposeAsyncImage(
         model = ImageRequest.Builder(LocalContext.current)
             .data(imageUrl)
@@ -174,14 +119,13 @@ fun PokemonImage(imageUrl: String, primaryType: String) {
             .diskCachePolicy(CachePolicy.ENABLED)
             .memoryCachePolicy(CachePolicy.ENABLED)
             .build(),
-        contentDescription = "pokemon image",
+        contentDescription = "Изображение покемона",
         contentScale = ContentScale.Crop,
         filterQuality = FilterQuality.None,
         modifier = Modifier
             .size(290.dp)
     )
 }
-
 
 @Composable
 fun PokemonInfoCard(
@@ -202,7 +146,7 @@ fun PokemonInfoCard(
                 style = MaterialTheme.typography.headlineLarge,
                 color = MaterialTheme.colorScheme.onSurface
             )
-            Spacer(modifier = Modifier.height(4.dp))
+            HorizontalDivider()
             content()
         }
     }
@@ -211,7 +155,7 @@ fun PokemonInfoCard(
 @Composable
 fun OverviewSection(pokemon: PokemonInfo, contentColor: Color) {
     Column(
-        verticalArrangement = Arrangement.spacedBy(5.dp)
+        verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         OverviewItem(
             icon = Icons.Default.Height,
@@ -233,11 +177,11 @@ fun OverviewSection(pokemon: PokemonInfo, contentColor: Color) {
         )
     }
 }
+
 @Composable
 fun OverviewItem(icon: ImageVector, label: String, value: String, contentColor: Color) {
     Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.padding(vertical = 4.dp)
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Icon(
             imageVector = icon,
@@ -249,29 +193,28 @@ fun OverviewItem(icon: ImageVector, label: String, value: String, contentColor: 
         Text(
             text = "$label: $value",
             fontSize = 18.sp,
-            fontWeight = FontWeight.Normal,
+            fontWeight = FontWeight.Medium,
             color = contentColor
         )
     }
 }
 
 @Composable
-fun StatsSection(stats: List<Stat>, contentColor: Color, onStatClick: (Stat) -> Unit) {
+fun StatsSection(stats: List<Stat>, contentColor: Color) {
     Column(
-        verticalArrangement = Arrangement.spacedBy(5.dp)
+        verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         stats.forEach { stat ->
-            StatItem(name = stat.name, value = stat.stat)
+            StatItem(name = stat.name, value = stat.stat, contentColor = contentColor)
         }
     }
 }
 
 @Composable
-fun StatItem(name: String, value: Int) {
+fun StatItem(name: String, value: Int, contentColor: Color) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 4.dp)
     ) {
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
@@ -280,12 +223,14 @@ fun StatItem(name: String, value: Int) {
             Text(
                 text = name.replaceFirstChar { it.uppercase() },
                 fontSize = 16.sp,
-                fontWeight = FontWeight.Medium
+                fontWeight = FontWeight.Medium,
+                color = contentColor
             )
             Text(
                 text = value.toString(),
                 fontSize = 16.sp,
-                fontWeight = FontWeight.Medium
+                fontWeight = FontWeight.Medium,
+                color = contentColor
             )
         }
         Spacer(modifier = Modifier.height(4.dp))
